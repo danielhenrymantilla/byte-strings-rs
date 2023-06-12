1220335816037f90b889bcd88ee59795b3ee24d1
Daniel Henry-Mantilla <daniel.henry.mantilla@gmail.com>
2023-06-13 01:29:47 +0200

    Add regression test against `RIGHT`

--- tests/consts.rs
+++ tests/consts.rs
@@ -2,10 +2,12 @@
 #[macro_use]
 extern crate byte_strings;
 
+const THIRD: &str = "";
+
 #[test]
 fn concat_bytes() {
     const GREETING: &str = "Hello";
-    const MESSAGE: &[u8; 13] = const_concat_bytes!(GREETING, ", World!");
+    const MESSAGE: &[u8; 13] = const_concat_bytes!(GREETING, ", World!", THIRD);
     assert_eq!(MESSAGE, b"Hello, World!");
 }
 
@@ -13,7 +15,7 @@ fn concat_bytes() {
 #[test]
 fn concat() {
     const GREETING: &str = "Hello";
-    const MESSAGE: &str = const_concat!(GREETING, ", World!");
+    const MESSAGE: &str = const_concat!(GREETING, ", World!", THIRD);
     assert_eq!(MESSAGE, "Hello, World!");
 }
 
@@ -22,7 +24,7 @@ fn c_str() {
     use ::byte_strings::const_;
 
     const MESSAGE: &str = "Hello, World!";
-    const C_MESSAGE: &const_::CStr = const_::c_str!(MESSAGE);
+    const C_MESSAGE: &const_::CStr = const_::c_str!(MESSAGE, THIRD);
     assert_eq!(
         C_MESSAGE.to_bytes_with_nul(),
         b"Hello, World!\0",
